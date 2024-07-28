9350f3162425e36a8011053aa42d70e04e6fa80f
Alessandro Sangalli <sangalli.alessandro@gmail.com>
2024-07-28 18:47:57 -0400

    Setup project

---
+++ WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace functional_core_imperative_shell_fsharp
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32.0 + (float this.TemperatureC / 0.5556)
